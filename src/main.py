704e0f25ac64ee07f9d24507e816408a593bb9ad
Your Name <nschoena@hotmail.com>
2026-02-05 11:27:52 -0600

    feat/scorecard create and display logic completed

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import sys
-from course import Course as C
-from score import Score as S
+from course import Course
+from score import Score
 from pathlib import Path
 from datetime import date, datetime
+from scorecard import ScoreCard
 
 """
 Main entry point for the Golf Score Tracker application
@@ -23,35 +24,18 @@ def main():
 COURSE_BASE_PATH = Path(__file__).resolve().parent.parent / "data" / "courses"
 SCORE_BASE_PATH = Path(__file__).resolve().parent.parent / "data" / "scores"
 
-# # Hawley test
-# # define the path to the course json file to load
-# course_file_to_load = COURSE_BASE_PATH / "hawley_white.json"
-# # load the json file into course object
-# hawley_white = C.load_from_json(course_file_to_load)
-# # display the course info
-# hawley_white.display_course_info_old()
-
-# Village Green test
-# define the path to the course json file to load
+# Village Green 
+# define the path to the course json file to load and load it
 course_file_to_load = COURSE_BASE_PATH / "village_green_white.json"
-# load the json file into course object
-village_green_white = C.load_from_json(course_file_to_load)
-# display the course info
-village_green_white.display_course_info()
+village_green_white_course = Course.load_from_json(course_file_to_load)
 
-# score test
-# 20251004_village_green_white test
-# define the path to the course json file to load
+# define the path to the score json file to load and load it
 score_file_to_load = SCORE_BASE_PATH / "20251004_village_green_white.json"
+village_green_white_20251004_score = Score.load_from_json(score_file_to_load)
 
-# load the json file into score object
-village_green_white_20251004_score = S.load_from_json(score_file_to_load)
-
-# display the score
-village_green_white_20251004_score.display_score_info()
-
-# use print statement to confirm the information in the score object is valid
-# print(village_green_white_20251004_score)
+# Use ScoreCard class to display the scorecard and additional analytics
+score = ScoreCard.load_scorecard(village_green_white_course, village_green_white_20251004_score)
+score.display_scorecard()
 
 # --- Script Execution Block ---
 if __name__ == "__main__":
